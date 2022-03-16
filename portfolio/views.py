c7224f2376d4b95a399e030fd990dccbf569f11c
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-03-15 21:26:56 -0300

    🚧 create initial templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the portfolio index.")
+def portfolio(request):
+    return render(request, 'portfolio.html', {})
